0e345eda117cc6fa844906b960963c6093d161d1
jakkari jaidiao <jakkarin.jaidiao2544@gmail.com>
2021-05-24 11:40:49 +0700

    commit

--- Login/register.php
+++ Login/register.php
@@ -10,7 +10,7 @@
 			</div>
 			<div class="login-menu">
 				<ul>
-					<li><a href="login.html">Login</a></li>
+					<li><a href="../Login/login.php">Login</a></li>
 				</ul>
 			</div>
 		</div>
@@ -24,44 +24,56 @@
 				<div class="col-md-6 col-lg-5">
 					<div class="register-box bg-white box-shadow border-radius-10">
 						<div class="wizard-content">
-							<form class="tab-wizard2 wizard-circle wizard">
-								<h5>Basic Account Credentials</h5>
+							<form class="tab-wizard2 wizard-circle wizard" action="check_register.php" method="post" name="registerform" onsubmit="return validateForm()">
+							<h5></h5>
 								<section>
 									<div class="form-wrap max-width-600 mx-auto">
 										<div class="form-group row">
 											<label class="col-sm-4 col-form-label">Email Address*</label>
 											<div class="col-sm-8">
-												<input type="email" class="form-control">
+												<input type="email" class="form-control" id="email" name="email">
+												<div id="email" class="text-danger"></div>
 											</div>
 										</div>
 										<div class="form-group row">
 											<label class="col-sm-4 col-form-label">Username*</label>
 											<div class="col-sm-8">
-												<input type="text" class="form-control">
+												<input type="text" class="form-control" id="username"name="username">
+												<div id="username" class="text-danger"></div>
 											</div>
 										</div>
 										<div class="form-group row">
 											<label class="col-sm-4 col-form-label">Password*</label>
 											<div class="col-sm-8">
-												<input type="password" class="form-control">
+												<input type="password" class="form-control" id="password"name="password">
+												<div id="password" class="text-danger"></div>
 											</div>
 										</div>
 										<div class="form-group row">
 											<label class="col-sm-4 col-form-label">Confirm Password*</label>
 											<div class="col-sm-8">
-												<input type="password" class="form-control">
+												<input type="password" class="form-control" id="conpassword"name="conpassword">
+												<div id="conpassword" class="text-danger"></div>
 											</div>
 										</div>
 									</div>
 								</section>
 								<!-- Step 2 -->
-								<h5>Personal Information</h5>
+								<h5></h5>
 								<section>
 									<div class="form-wrap max-width-600 mx-auto">
 										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">Full Name*</label>
+											<label class="col-sm-4 col-form-label">First Name*</label>
 											<div class="col-sm-8">
-												<input type="text" class="form-control">
+												<input type="text" class="form-control" id="fname"name="fname">
+												<div id="fname" class="text-danger"></div>
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-4 col-form-label">Last Name*</label>
+											<div class="col-sm-8">
+												<input type="text" class="form-control" id="lname"name="lname">
+												<div id="lname" class="text-danger"></div>
 											</div>
 										</div>
 										<div class="form-group row align-items-center">
@@ -75,94 +87,30 @@
 													<input type="radio" id="female" name="gender" class="custom-control-input">
 													<label class="custom-control-label" for="female">Female</label>
 												</div>
+												<div id="gender" class="text-danger"></div>
 											</div>
 										</div>
 										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">City</label>
+											<label class="col-sm-4 col-form-label">Phone Number*</label>
 											<div class="col-sm-8">
-												<input type="text" class="form-control">
+												<input type="text" class="form-control" id="phonenumber"name="phonenumber">
+												<div id="phonenumber" class="text-danger"></div>
 											</div>
 										</div>
 										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">State</label>
+											<label class="col-sm-4 col-form-label">Address</label>
 											<div class="col-sm-8">
-												<input type="text" class="form-control">
+												<input type="text" class="form-control" id="address"name="address">
+												<div id="address" class="text-danger"></div>
 											</div>
 										</div>
 									</div>
 								</section>
 								<!-- Step 3 -->
-								<h5>Payment Method & Info</h5>
+								<h5></h5>
 								<section>
 									<div class="form-wrap max-width-600 mx-auto">
-										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">Credit Card Type</label>
-											<div class="col-sm-8">
-												<select class="form-control selectpicker" title="Select Card Type">
-													<option value="1">Option 1</option>
-													<option value="2">Option 2</option>
-													<option value="3">Option 3</option>
-												</select>
-											</div>
-										</div>
-										<div class="form-group row align-items-center">
-											<label class="col-sm-4 col-form-label">Credit Card Number</label>
-											<div class="col-sm-8">
-												<input type="text" class="form-control">
-											</div>
-										</div>
-										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">CVC</label>
-											<div class="col-sm-3">
-												<input type="text" class="form-control">
-											</div>
-										</div>
-										<div class="form-group row">
-											<label class="col-sm-4 col-form-label">Expiration Date</label>
-											<div class="col-sm-8">
-												<div class="row">
-													<div class="col-6">
-														<select class="form-control selectpicker" title="Month" data-size="5">
-															<option value='01'>January</option>
-															<option value='02'>February</option>
-															<option value='03'>March</option>
-															<option value='04'>April</option>
-															<option value='05'>May</option>
-															<option value='06'>June</option>
-															<option value='07'>July</option>
-															<option value='08'>August</option>
-															<option value='09'>September</option>
-															<option value='10'>October</option>
-															<option value='11'>November</option>
-															<option value='12'>December</option>
-														</select>
-													</div>
-													<div class="col-6">
-														<select class="form-control selectpicker" title="Year" data-size="5">
-															<option>2020</option>
-															<option>2019</option>
-															<option>2018</option>
-															<option>2017</option>
-															<option>2016</option>
-															<option>2015</option>
-															<option>2014</option>
-															<option>2013</option>
-															<option>2012</option>
-															<option>2011</option>
-															<option>2010</option>
-															<option>2009</option>
-														</select>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</section>
-								<!-- Step 4 -->
-								<h5>Overview Information</h5>
-								<section>
-									<div class="form-wrap max-width-600 mx-auto">
-										<ul class="register-info">
+									<ul class="register-info">
 											<li>
 												<div class="row">
 													<div class="col-sm-4 weight-600">Email Address</div>
@@ -198,7 +146,6 @@
 											<input type="checkbox" class="custom-control-input" id="customCheck1">
 											<label class="custom-control-label" for="customCheck1">I have read and agreed to the terms of services and privacy policy</label>
 										</div>
-									</div>
 								</section>
 							</form>
 						</div>
@@ -223,5 +170,107 @@
 			</div>
 		</div>
 	</div>
+
+	<script>
+    function validateForm() {
+        var email   	= document.forms["registerform"]["email"].value;
+        var username    = document.forms["registerform"]["username"].value;
+        var password    = document.forms["registerform"]["password"].value;
+        var conpassword = document.forms["registerform"]["conpassword"].value;
+		var fname  	 	= document.forms["registerform"]["fname"].value;
+		var lname  	 	= document.forms["registerform"]["lname"].value;
+		var gender  	= document.forms["registerform"]["gender"].value;
+		var phonenumber = document.forms["registerform"]["phonenumber"].value;
+		var address 	= document.forms["registerform"]["address"].value;
+
+		if (email == "") {
+        document.getElementById("email").innerHTML="<b> Please Your Enter Email ";
+        document.forms["registerform"]["email"].value ="";
+        document.forms["registerform"]["email"].focus();
+    return false;      
+    }else{
+    document.getElementById("email").innerHTML="";
+  }
+
+        if (username.length<5) {
+        document.getElementById("username").innerHTML="<b> username more 5 letters";
+        document.forms["registerform"]["username"].value ="";
+        document.forms["registerform"]["username"].focus();
+    return false;
+  }else{
+    document.getElementById("username").innerHTML="";
+  }
+
+        if (password.length<8) {
+        document.getElementById("password").innerHTML="<b> password more 8 letters";
+        document.forms["registerform"]["password"].value ="";
+        document.forms["registerform"]["password"].focus();
+    return false;
+  }else{
+    document.getElementById("password").innerHTML="";
+  }
+
+  		if (conpassword.length<8) {
+        document.getElementById("conpassword").innerHTML="<b> password more 8 letters";
+        document.forms["registerform"]["conpassword"].value ="";
+        document.forms["registerform"]["conpassword"].focus();
+    return false;
+  }else{
+    document.getElementById("conpassword").innerHTML="";
+  }
+
+  		if (password != conpassword) {
+        document.getElementById("conpassword").innerHTML="<b> Passwords do not match";
+        document.forms["registerform"]["password"].value ="";
+        document.forms["registerform"]["conpassword"].value ="";
+        document.forms["registerform"]["password"].focus();
+    return false;
+  }else{
+    document.getElementById("conpassword").innerHTML="";
+  }
+
+  		if (fname == "") {
+        document.getElementById("fname").innerHTML="<b> Please Your Enter full name ";
+        document.forms["registerform"]["fname"].value ="";
+        document.forms["registerform"]["fname"].focus();
+    return false;      
+    }else{
+    document.getElementById("fname").innerHTML="";
+  }
+  if (lname == "") {
+        document.getElementById("lname").innerHTML="<b> Please Your Enter Last name ";
+        document.forms["registerform"]["lname"].value ="";
+        document.forms["registerform"]["lname"].focus();
+    return false;      
+    }else{
+    document.getElementById("lname").innerHTML="";
+  }
+  if (gender == "") {
+        document.getElementById("gender").innerHTML="<b> Please Your select gender ";
+        document.forms["registerform"]["gender"].value ="";
+        document.forms["registerform"]["gender"].focus();
+    return false;      
+    }else{
+    document.getElementById("gender").innerHTML="";
+  }
+  		if (phonenumber == "") {
+        document.getElementById("phonenumber").innerHTML="<b> Please Your Enter Phone Number ";
+        document.forms["registerform"]["phonenumber"].value ="";
+        document.forms["registerform"]["phonenumber"].focus();
+    return false;      
+    }else{
+    document.getElementById("phonenumber").innerHTML="";
+  }
+
+  		if (address == "") {
+        document.getElementById("address").innerHTML="<b> Please Your Enter Address ";
+        document.forms["registerform"]["address"].value ="";
+        document.forms["registerform"]["address"].focus();
+    return false;      
+    }else{
+    document.getElementById("address").innerHTML="";
+  }
+    }
+  </script>
 
 <?php include "../include/footer_register.html"; ?> 
