7e90f1ce6320c7929421b68bddace003b805731e
jakkari jaidiao <jakkarin.jaidiao2544@gmail.com>
2021-05-27 09:07:59 +0700

    commit

--- sql/check_login.php
+++ sql/check_login.php
@@ -33,11 +33,11 @@ session_start();
                       }
 
                   }else{
-                    echo "sweetAlert('Erro!', 'Link inválido ou fora do prazo de validade.', 'error')";
-                    //echo "<script>";
-                      //echo "alert(\" user หรือ  password ไม่ถูกต้อง\");"; 
-                       //echo "window.history.back()";
-                    //echo "</script>";
+
+                    echo "<script>";
+                    echo "alert(\" user หรือ  password ไม่ถูกต้อง\");"; 
+                    echo "window.history.back()";
+                    echo "</script>";
                   }
 
         }else{
