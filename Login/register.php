fd09c57984e9f6e38a3a373dc2761d17ea01e2b0
jakkari jaidiao <jakkarin.jaidiao2544@gmail.com>
2021-05-27 15:24:03 +0700

    commit

--- Login/register.php
+++ Login/register.php
@@ -76,15 +76,6 @@
 								</div>
 							</div>
 							<div class="row pb-30">
-								<div class="col-6">
-									<div class="custom-control custom-checkbox">
-										<input type="checkbox" class="custom-control-input" id="customCheck1">
-										<label class="custom-control-label" for="customCheck1">Remember</label>
-									</div>
-								</div>
-								<div class="col-6">
-									<div class="forgot-password"><a href="../Error file/404.html">Forgot Password</a></div>
-								</div>
 							</div>
 							<div class="row">
 								<div class="col-sm-12">
