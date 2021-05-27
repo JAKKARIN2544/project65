37e796e11d758e91fa940497a24aff1c2cfc384e
jakkari jaidiao <jakkarin.jaidiao2544@gmail.com>
2021-05-27 09:51:14 +0700

    commit

--- sql/check_login.php
+++ sql/check_login.php
@@ -5,6 +5,21 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <!-- Site favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="../vendors/images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../vendors/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../vendors/images/favicon-16x16.png">
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="../vendors/styles/core.css">
+	<link rel="stylesheet" type="text/css" href="../vendors/styles/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="../src/plugins/sweetalert2/sweetalert2.css">
+	<link rel="stylesheet" type="text/css" href="../vendors/styles/style.css">
 </head>
 <body>
 <?php 
@@ -43,9 +58,13 @@ session_start();
 
                   }else{
                     echo '<script type="text/javascript">
-                          swal("", "ข้อมูลไม่ถูกต้อง", "error");
+                          <div id ="sa-error"> </div>;
                           </script>';
                     echo '<meta http-equiv="refresh" content="1.5;url=../Login/login.php" />';
+                    //echo '<script type="text/javascript">
+                          //swal("Oops...", "Incorrect username or password !!!", "error");
+                          //</script>';
+                    //echo '<meta http-equiv="refresh" content="1.5;url=../Login/login.php" />';
                   }
         }else{
 
@@ -54,5 +73,14 @@ session_start();
 
         }
 ?>
+<!-- js -->
+<script src="../vendors/scripts/core.js"></script>
+	<script src="../vendors/scripts/script.min.js"></script>
+	<script src="../vendors/scripts/process.js"></script>
+	<script src="../vendors/scripts/layout-settings.js"></script>
+
+	<!-- add sweet alert js & css in footer -->
+	<script src="../src/plugins/sweetalert2/sweetalert2.all.js"></script>
+	<script src="../src/plugins/sweetalert2/sweet-alert.init.js"></script>
 </body>
 </html>
