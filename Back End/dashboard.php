c34df8fa525ff2666d63e3eca22ad156aef96c2a
jakkari jaidiao <jakkarin.jaidiao2544@gmail.com>
2021-05-29 11:19:30 +0700

    commit

--- Back End/dashboard.php
+++ Back End/dashboard.php
@@ -18,7 +18,7 @@
 					<div class="card-box height-100-p widget-style3">
 						<div class="d-flex flex-wrap">
 							<div class="widget-data"> 
-								<div class="weight-700 font-24 text-dark"><?php echo $row['COUNT(u_id)'] ?></div>
+								<div class="weight-700 font-24 text-dark"><?php echo $row['COUNT(id)'] ?></div>
 								<div class="font-14 text-secondary weight-500">Customer</div>
 							</div>
 							<div class="widget-icon">
